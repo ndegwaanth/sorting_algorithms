5f3f040473aabc73892bb92ca11a2cfe4c77e6cb
Anthony <ndegwaanthony300@gmail.com>
2023-11-21 09:34:42 -0800

    modifief thebubble filee

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,7 +17,8 @@ void print_my_array(int n)
 	putchar('0' + n % 10);
 }
 /**
- * bubble_sort - this prototype is used to sort an array in ascending order using bubble sort
+ * bubble_sort - this prototype is used to sort
+ * an array in ascending order using bubble sort
  * @array: this is the array that is going to be sorted out
  * @size: this the length os the array
  * Return: return the array
@@ -29,13 +30,13 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i <= size - 1; i++)
 	{
-		for (j = 0; j <= size -1 - i; j++)
+		for (j = 0; j <= size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1 ] = temp;
+				array[j + 1] = temp;
 				for (k = 0; k < size; k++)
 				{
 					print_my_array(array[k]);
